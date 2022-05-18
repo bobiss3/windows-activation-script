0231caa1469ae123849ac61ce92d34bb7bdc9ea0
The-anonym-dev <105820875+The-anonym-dev@users.noreply.github.com>
2022-05-18 22:20:17 +0200

    windows 10 home activator

---
+++ Windows 10/home/windows-10-home_activator1.0.bat
@@ -0,0 +1,6 @@
+slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+timeout 100
+slmgr /skms kms.digiboy.ir
+timeout 100
+slmgr /ato
+timeout 100
---
+++ Windows 10/home/windows-10-home_activator1.1.bat
@@ -0,0 +1,17 @@
+echo Hola, bienvenido a WINDOWS ACTIVATOR by Anonymus. 
+timeout 20
+echo Version 1.1 para windows-10-home
+timeout 20
+echo NOTA: Activar este script con adminitrado o dara error
+timeout 20
+echo Si aparecen ventanas emergente pulse ''aceptar''
+timeout 20
+slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+timeout 100
+slmgr /skms kms.digiboy.ir
+timeout 100
+slmgr /ato
+timeout 100
+echo WINDOWS ACTIVADO :D
+timeout 20
+exit
