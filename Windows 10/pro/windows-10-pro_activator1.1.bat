a18535a66267fdb707ac114ff93c7a428cc0277e
The-anonym-dev <105820875+The-anonym-dev@users.noreply.github.com>
2022-05-18 22:25:27 +0200

    Create windows-10-pro_activator1.1.bat

---
+++ Windows 10/pro/windows-10-pro_activator1.1.bat
@@ -0,0 +1,17 @@
+echo Hola, bienvenido a WINDOWS ACTIVATOR by Anonymus-dev. 
+timeout 20
+echo Version 1.1 para windows-10-pro
+timeout 20
+echo NOTA: Activar este script con adminitrado o dara error
+timeout 20
+echo Si aparecen ventanas emergente pulse ''aceptar''
+timeout 20
+slmgr /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
+timeout 100
+slmgr /skms kms.digiboy.ir
+timeout 100
+slmgr /ato
+timeout 100
+echo WINDOWS ACTIVADO :D
+timeout 20
+exit
